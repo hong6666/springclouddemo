3429fe752dadb2d1ab73e67969943a38c70133c8
lihonghao <1345455432@qq.com>
2020-04-15 10:37:55 +0800

    创建服务消费者，修改eureka-client为原来的

---
+++ eureka-consumer/src/test/java/com/life/consumer/eurekaconsumer/EurekaConsumerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.life.consumer.eurekaconsumer;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
